14dcaaf03bfa5574cb9f5d0246eb09d0226c9f7f
mjoghenemaega <mjoghenemaega@gmail.com>
2022-10-24 18:07:33 +0100

    added smartcontact page

---
+++ new-busterapp/assets/js/slider.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".sidebar-form .call-action").click(function(){
+            $(this).parents(".sidebar-form").toggleClass("show");
+    });
+});
+
+
+$(document).ready(function(){
+    $("#one").click(function(){
+            $(this).parents(".sidebar-form").toggleClass("show");
+    });
+});
+
+
+
+
